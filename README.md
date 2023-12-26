72db7f259995d7d76035117c178b37e33f6eaea4
Tom Flaherty <tom.flaherty03@gmail.com>
2023-12-26 13:26:31 +0000

    Minimum working prototype!

---
+++ README.md
@@ -0,0 +1,64 @@
+This is based on a YouTube video by digikey, although the video is a little
+out of date due to some changes to the repo they were using.
+
+https://www.youtube.com/watch?v=u-vmsIr-s7w&t=541s
+
+How I got this to work:
+
+Copy the consicely named no-OS-FatFS-SD-SPI-Rpi-Pico from here:
+
+```git clone https://github.com/carlk3/no-OS-FatFS-SD-SPI-Rpi-Pico.git```
+```https://github.com/carlk3/no-OS-FatFS-SD-SPI-RPi-Pico```
+
+This is the latest hash when I cloned: 196016f
+
+And the date is 26/12/2023
+
+# Note
+
+There are some warnings when compiling the no-OS-FatFS-SD-SPI-Rpi-Pico repo, so
+you should not use -Werror when compiling.
+
+I believe the no-OS-FatFS-SD-SPI-Rpi-Pico repo is based on 
+http://elm-chan.org/fsw/ff/00index_e.html
+
+So you can look there for the f_open type commands.
+
+# How to get it working
+
+Copy simple_example/hw_config.c into FatFs_SPI/sd_driver.
+This file is needed for the SD card config.
+
+Within hw_config.c, modify the spi_t struct:
+- .hw_inst = spi1, // I used spi1 instead of spi0
+- .miso_gpio = 8,  // GPIO number (not Pico pin number)
+- .mosi_gpio = 11,
+- .sck_gpio = 10,
+- .baud_rate = 1000 * 1000 // You can probably increase this later, but this is a good starting point
+
+Next, still within hw_config.c, set .ss_gpio = 9, and set .use_card_detect = false
+
+Note the value of .pcName (usually "0:") as this will be needed for mounting.
+
+Next, you need to add the hw_config.c file to the CMakeLists.txt.
+Go to FatFs_SPI/CMakeLists.txt, and uncomment the following line:
+
+```${CMAKE_CURRENT_LIST_DIR}/sd_driver/hw_config.c```
+
+Now in the root of your project, in source/CMakeLists.txt, you need to add the 
+FatFs_SPI library using
+
+```add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/no-OS-FatFS-SD-SPI-Rpi-Pico/FatFs_SPI build)```
+
+And link it to your project
+
+```
+target_link_libraries(${PROJECT_NAME}
+    pico_stdlib
+    pico_cyw43_arch_none
+    FatFs_SPI
+    # You'll need to link other libraries for other pico functions
+    )
+```
+
+And remember to disable -Werror
--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,154 @@
 #include <stdio.h>
 
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "pico/cyw43_arch.h"
+
+#include "sd_card.h"
+#include "ff.h"
+
+static inline uint64_t millis();
+static inline uint64_t micros();
 
 int main( void )
 {
+    uint64_t bootTime = millis();
     stdio_init_all();
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
     }
+    while( millis() - bootTime < 1000ULL )
+    {
+        printf( "Booting\n" );
+        sleep_ms( 100 );
+    }
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+
+    FRESULT fr;
+    FATFS fs;
+    FIL fil;
+    int ret;
+    char buf[100];
+    char filename[] = "tomstest.txt";
+
+    // Initialize the SD casd
+    if( !sd_init_driver() )
+    {
+        printf( "Couldn't initialise the SD card :(\n" );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "SD card initialised ok!\n" );
+
+    // Mount the SD card
+    fr = f_mount( &fs, "0:", 1 ); // "0:" must be the same in the hw_config.c file
+    if( fr != FR_OK )
+    {
+        printf( "ERROR: Couldn't mount filesystem (%d)\r\n", fr );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "SD card mounted ok!\n" );
+
+    // Open file for writing
+    fr = f_open( &fil, filename, FA_WRITE | FA_CREATE_ALWAYS );
+    if( fr != FR_OK )
+    {
+        printf( "Error with f_open\n" );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "File opened ok!\n" );
+
+    // Write stuff to the file
+    ret = f_printf( &fil, "This is my test string.\r\n");
+    if( ret < 0 )
+    {
+        printf( "ERROR: Could not write to the file" );
+        f_close( &fil );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "Write operation successful!\n" );
+
+    ret = f_printf( &fil, "Spud is a noisy bird.\r\n");
+    if( ret < 0 )
+    {
+        printf( "ERROR: Could not write to the file" );
+        f_close( &fil );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "Write operation successful!\n" );
+
+    // Close the file (very important)
+    fr = f_close( &fil );
+    if( fr != FR_OK )
+    {
+        printf( "Error, couldn't close the file" );
+        f_close( &fil );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "File closed ok!\n" );
+
+    // Open the file again to read
+    fr = f_open( &fil, filename, FA_READ );
+    if( fr != FR_OK )
+    {
+        printf( "Error, couldn't open the file for reading" );
+        f_close( &fil );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "File reopened ok!\n" );
+    
+    // Read the contents of the file
+    printf( "Reading from file %s:\r\n", filename );
+    printf( "---\r\n" );
+    while( f_gets( buf, sizeof(buf), &fil ) )
+    {
+        printf( buf );
+    }
+    printf( "---\r\n" );
+
+    fr = f_close( &fil );
+    if( fr != FR_OK )
+    {
+        printf( "Error, couldn't close the file" );
+        f_close( &fil );
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        while( true );
+    }
+    else
+        printf( "File closed ok!\n" );
+
+    f_unmount( "0:" );
+    printf( "File closed ok!\n" );
+
+    // If you see flashing lights, no errors!
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(250);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(250);
     }
+}
 
-    for( ;; ) {}
+static inline uint64_t millis()
+{
+    return micros() / 1000ULL;
+}
+
+static inline uint64_t micros()
+{
+    return to_us_since_boot( get_absolute_time() );
 }
